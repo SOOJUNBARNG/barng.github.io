ae64ab0030ebec77fe574a1c2c8ec8684baf7615
BarngSJ <ban.sujun@medical-frontier.jp>
2025-05-13 15:24:52 +0900

    add_script

--- asset/script.js
+++ asset/script.js
@@ -13,3 +13,14 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+// Load header dynamically
+fetch('../asset/header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header-placeholder').innerHTML = data)
+    .catch(error => console.error('Error loading the header:', error));
+
+fetch('../asset/language_switcher.html')
+.then(response => response.text())
+.then(data => document.getElementById('language-switcher').innerHTML = data)
+.catch(error => console.error('Error loading the header:', error));
