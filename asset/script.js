e4cc33290e60c8ef5cb0756065ec5e405256a7b0
BarngSJ <ban.sujun@medical-frontier.jp>
2025-05-22 18:26:38 +0900

    change_for_web

--- asset/script.js
+++ asset/script.js
@@ -1,23 +1,3 @@
-// 折りたたみ可能状態
-document.addEventListener("DOMContentLoaded", () => {
-    const headers = document.querySelectorAll(".foldable-header");
-    headers.forEach(header => {
-        header.style.cursor = "pointer";
-        header.addEventListener("click", () => {
-        const content = header.nextElementSibling;
-        if (content && content.classList.contains("foldable-content")) {
-            content.style.display = content.style.display === "none" ? "block" : "none";
-        }
-        });
-
-        // Optionally hide all content by default
-        const content = header.nextElementSibling;
-        if (content && content.classList.contains("foldable-content")) {
-        content.style.display = "none";
-        }
-    });
-});
-
 // Load header dynamically
 fetch('../asset/header.html')
     .then(response => response.text())
