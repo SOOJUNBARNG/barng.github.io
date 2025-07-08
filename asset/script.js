3fce2e1c70a097ab098dc4a38b6238bd0421a789
BARNG_private_mac <powwerer@gmail.com>
2025-07-08 15:32:42 +0900

    add_script

--- asset/script.js
+++ asset/script.js
@@ -1,10 +1,10 @@
 // Load header dynamically
-fetch('../asset/header.html')
+fetch('/barng.github.io/asset/header.html')
     .then(response => response.text())
     .then(data => document.getElementById('header-placeholder').innerHTML = data)
     .catch(error => console.error('Error loading the header:', error));
 
-fetch('../asset/language_switcher.html')
+fetch('/barng.github.io/asset/language_switcher.html')
 .then(response => response.text())
 .then(data => document.getElementById('language-switcher').innerHTML = data)
 .catch(error => console.error('Error loading the header:', error));
