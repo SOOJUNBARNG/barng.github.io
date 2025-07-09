447805829a738e6f0d7b777b508d54c1804ca73e
ban.sujun <ban.sujun@medical-frontier.jp>
2025-07-09 13:09:28 +0900

    new

--- asset/script.js
+++ asset/script.js
@@ -8,3 +8,27 @@ fetch('/barng.github.io/asset/language_switcher.html')
     .then(response => response.text())
     .then(data => document.getElementById('language-switcher').innerHTML = data)
     .catch(error => console.error('Error loading the header:', error));
+
+// Foldable
+document.addEventListener('DOMContentLoaded', function() {
+    // Find all the foldable headers
+    const foldableHeaders = document.querySelectorAll('.foldable-header');
+    
+    foldableHeaders.forEach(header => {
+        // Add a click event listener to each header
+        header.addEventListener('click', function() {
+        // Toggle the 'active' class on the header
+        this.classList.toggle('active');
+    
+        // Get the content element, which is the next element sibling
+        const content = this.nextElementSibling;
+    
+        // Check if the content is currently shown or hidden and toggle it
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+        });
+    });
+    });
